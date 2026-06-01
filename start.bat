087a27974a3e89cae9b8bd5d0f57d4f80244099e
Lucas Cardoso <lucasoliveirag2025@gmail.com>
2026-06-01 16:44:29 -0300

    rename main html to index.html for github pages root

--- start.bat
+++ start.bat
@@ -4,5 +4,5 @@ cd /d "%~dp0"
 echo Subindo servidor de waveform (mantenha esta janela aberta enquanto usar o gerador)...
 start "underscore server" cmd /k "python server.py"
 timeout /t 2 /nobreak >nul
-start "" "underscore-gerador.html"
+start "" "index.html"
 exit
